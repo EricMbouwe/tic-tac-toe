fb56dc8c9cc5709fdbc78a12a5e69a11099a7223
ericmbouwe <ericmbouwe@yahoo.fr>
2020-04-27 09:07:57 +0100

    check move and check_positions methods added

--- lib/Game.rb
+++ lib/Game.rb
@@ -1,30 +1,52 @@
 require_relative 'player'
 
 class Game
-	attr_accessor :board
+  attr_accessor :board
 
-	def initialize(player1, player2)
-		@player1 = Player.new
-		@player2 = Player.new
-		@board = [
-  ['-------------'],
-  ['|', '   ', '|', '   ', '|', '   ', '|'],
-  ['-------------'],
-  ['|', '   ', '|', '   ', '|', '   ', '|'],
-  ['-------------'],
-  ['|', '   ', '|', '   ', '|', '   ', '|'],
-  ['-------------']
-]
+  def initialize(_player1, _player2)
+    @player1 = Player.new
+    @player2 = Player.new
+    @board = [
+      ['-------------'],
+      ['|', '   ', '|', '   ', '|', '   ', '|'],
+      ['-------------'],
+      ['|', '   ', '|', '   ', '|', '   ', '|'],
+      ['-------------'],
+      ['|', '   ', '|', '   ', '|', '   ', '|'],
+      ['-------------']
+    ]
+  end
+
+  def choose_symbol(choice, player1, player2)
+    player1.sym = choice
+    if choice == 'X'
+      player2.sym = 'O'
+    elsif choice == 'O'
+      player2.sym = 'X'
+    end
+  end
+
+  def check_move(gamebord, position)
+	while pos < 1 || pos > 9
+		'Wrong position given, please enter a number between 1 and 9'
 	end
+	position
+  end
 
-	def choose_symbol(choice, player1, player2)
-		player1.sym = choice 
-			if choice == 'X'
-				player2.sym = 'O'
-			elsif choice == 'O'
-				player2.sym = 'X'
-			end
+  def check_taken_position(gameboard, position, player1, player2)
+	while player1.positions.include?(position) || player2.positions.include?(position)
+		'position already taken, please enter a new one'
 	end
+	position
+  end
 
+  def winner(winning)
+	winning.each do |cases|
+	  return "Congratulations #{player1.capitalize}, you win!!!" if (cases - player1.positions).empty?
+	  return "Congratulations #{player2.capitalize}, you win!!!" if (cases - player2.positions).empty?
+	  return 'draw game (:'.upcase if player1.positions.size + player2.positions.size == 9
+	end
+	''
+  end
 
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,35 +1,35 @@
 class Player
-	attr_accessor :name, :positions, :sym
+  attr_accessor :name, :positions, :sym
 
-	def initialize
-		@name = name
-		@positions = []
-		@sym = ''
-	end
+  def initialize
+    @name = name
+    @positions = []
+    @sym = ''
+  end
 
-	def move(gameboard, position, sym)
-		positions.push(position)
+  def move(gameboard, position, sym)
+    positions.push(position)
 
-		if position == 1
-	    gameboard[1][1] = sym
-	  elsif position == 2
-	    gameboard[1][3] = sym
-	  elsif position == 3
-	    gameboard[1][5] = sym
-	  elsif position == 4
-	    gameboard[3][1] = sym
-	  elsif position == 5
-	    gameboard[3][3] = sym
-	  elsif position == 6
-	    gameboard[3][5] = sym
-	  elsif position == 7
-	    gameboard[5][1] = sym
-	  elsif position == 8
-	    gameboard[5][3] = sym
-	  elsif position == 9
-	    gameboard[5][5] = sym
-	  else
-	    puts 'Wrong positionition given, enter a number between 1 and 9'
-	  end
-	end
+    if position == 1
+      gameboard[1][1] = sym
+    elsif position == 2
+      gameboard[1][3] = sym
+    elsif position == 3
+      gameboard[1][5] = sym
+    elsif position == 4
+      gameboard[3][1] = sym
+    elsif position == 5
+      gameboard[3][3] = sym
+    elsif position == 6
+      gameboard[3][5] = sym
+    elsif position == 7
+      gameboard[5][1] = sym
+    elsif position == 8
+      gameboard[5][3] = sym
+    elsif position == 9
+      gameboard[5][5] = sym
+    else
+      puts 'Wrong positionition given, enter a number between 1 and 9'
+    end
+  end
 end
