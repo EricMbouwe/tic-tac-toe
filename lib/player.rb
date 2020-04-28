c2a73a2223997f18e9bed21bc0dded4f2fdffc0f
abrormukimov <abrormm34@gmail.com>
2020-04-28 10:29:18 +0500

    Updated rubocop and Fix the errors. Modified main file

--- lib/player.rb
+++ lib/player.rb
@@ -9,27 +9,15 @@ class Player
 
   def move(gameboard, position, sym)
     positions.push(position)
-
-    if position == 1
-      gameboard[1][1] = " #{sym} "
-    elsif position == 2
-      gameboard[1][3] = " #{sym} "
-    elsif position == 3
-      gameboard[1][5] = " #{sym} "
-    elsif position == 4
-      gameboard[3][1] = " #{sym} "
-    elsif position == 5
-      gameboard[3][3] = " #{sym} "
-    elsif position == 6
-      gameboard[3][5] = " #{sym} "
-    elsif position == 7
-      gameboard[5][1] = " #{sym} "
-    elsif position == 8
-      gameboard[5][3] = " #{sym} "
-    elsif position == 9
-      gameboard[5][5] = " #{sym} "
-    else
-      'Wrong position given, enter a number between 1 and 9'
-    end
+    return gameboard[1][1] = " #{sym} " if position == 1
+    return gameboard[1][3] = " #{sym} " if position == 2
+    return gameboard[1][5] = " #{sym} " if position == 3
+    return gameboard[3][1] = " #{sym} " if position == 4
+    return gameboard[3][3] = " #{sym} " if position == 5
+    return gameboard[3][5] = " #{sym} " if position == 6
+    return gameboard[5][1] = " #{sym} " if position == 7
+    return gameboard[5][3] = " #{sym} " if position == 8
+    return gameboard[5][5] = " #{sym} " if position == 9
+    return 'Wrong position given, enter a number between 1 and 9' if position < 1 || position > 9
   end
 end
