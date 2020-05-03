b00e2bc8deab93eb61109ed40f9132c9c2fa6f22
ericmbouwe <ericmbouwe@yahoo.fr>
2020-05-03 14:41:59 +0100

    latest tests added for player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -43,11 +43,18 @@ describe "Player" do
 		let(:gameboard) { Array.new(7) {Array.new(3)} }
 		let(:position) { 8 }
 		let(:sym) { 'x' }
-		it "should accept 3 arguments: gameboard, position, sym" do
-			expect(player.move).to receive(:gameboard).with('User Action')
-			expect(player.move).to receive(:position).with('User Action')
-			expect(player.move).to receive(:sym).with('User Action') 
+		
+		it 'should takes 3 arguments when called' do
+			allow(player).to receive(:move).with(gameboard, position, sym)
 		end
+
+		it 'should place the symbol at the postion given in the corresponding gameboard index' do
+			expect(player.move(gameboard, 5, 'X')).to eq(gameboard[3][3] = ' X ')
+		 end
+ 
+		 it 'should place the symbol at the postion given in the corresponding gameboard index' do
+			 expect(gameboard[1][3] = ' O ').to eq(player.move(gameboard, 2, 'O'))
+		 end
 
 	end
 end
