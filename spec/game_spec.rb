5216e4a4ec330d833837ddb38c80c9ecb34d6617
abrormukimov <abrormm34@gmail.com>
2020-05-04 20:47:06 +0500

    Add bad_play and position taken to rspec file

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,6 +3,10 @@ require 'player'
 
 describe 'Game' do
   game = Game.new
+  player1 = Player.new
+  player2 = Player.new
+  let(:choice) { 'O' }
+  let(:position) { 1 }
   let(:gameboard) {
     [
       ['-------------'],
@@ -14,25 +18,16 @@ describe 'Game' do
       ['-------------']
     ]
   }
-  let(:win_arr) { [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]] }
+  let(:winning) { [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]] }
+
   describe '#initialize' do
     it 'should create an object without arguments' do
       expect(game.board).to eq(gameboard)
-      expect(game.winning).to eq(win_arr)
+      expect(game.winning).to eq(winning)
     end
   end
 
-  # describe "#add_player" do
-  #     it 'should create a new instance of player' do
-  #       expect(game.add_player).to be(Player.new)
-  #     end
-  # end
-
   describe "#choose_symbol" do
-    player1 = Player.new
-    player2 = Player.new
-    let(:choice) { 'O' }
-
     it 'should set a sym to player1' do
       game.choose_symbol(choice, player1, player2)
       expect(player1.sym).to eql('O')
@@ -55,4 +50,37 @@ describe 'Game' do
       expect(game.valid_move?(pos2)).to be(false)
     end
   end
+
+  describe "#position_taken?" do
+    it "should take position of both players and players as arguments and check if the position is taken" do
+      game.position_taken?(position, player1, player2)
+      player1.positions.push(position)
+      player2.positions.push(position)
+      player1.positions.each do |number|
+        expect(game.position_taken?(number, player1, player2)).to be true
+      end
+    end
+  end
+
+  describe "#bad_play?" do
+    let(:pos) { 11 }
+    it "should check if the given numbers are between 1 and 9 and also check if position is not already taken" do
+      expect(game.valid_move?(position)).to be true
+      expect(game.valid_move?(pos)).to be false
+      expect(game.position_taken?(position, player1, player2)).to be true  
+    end   
+  end
+
+  describe "#winner" do
+    it "should accep winning and player as arguments and to check if a player won or not" do
+      game.winner(winning, player1)
+      player1.positions.push(1)
+      player1.positions.push(2)
+      player1.positions.push(3)
+      winning.each do |winning_conds|
+        expect(player1.positions.include?(winning_conds[0]) && player1.positions.include?(winning_conds[1]) && player1.positions.include?(winning_conds[2])).to be true
+      end
+    end
+  end
 end
+
